83e4b0a22e4ed9a7e2df6f0b7291aba63bf9cb6a
wordstat9 <53854303+wordstat9@users.noreply.github.com>
2019-08-17 12:51:53 +0300

    final

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-advice_url = "/api/forecasts"
+advice_url = "https://sf-pyw.mosyag.in/m04/api/forecasts"
 function take_predict() {
     $.getJSON(advice_url, function(data) {
         $.each(data["prophecies"], function(i, d) {
